eb7f981458298fae90ad99e6194ffd12f44742dc
Gitter <40431840+gitter-ram@users.noreply.github.com>
2019-04-15 16:51:11 +0530

    Create CLIClient.py

---
+++ CLIClient.py
@@ -0,0 +1,18 @@
+#The command line based liabrarian client interface
+#intro
+def doIntro():
+  print("********Welcome**********")
+  print("This is Liabrarian v.1.0")
+  print("************************")
+  print("created by: Harkishan Singh ,Harshvardhan and Vaibhav Maitry")
+  print("------------------------------------------------------------")
+
+def doMainMenu():
+  print("Main Menu:")
+  print("---------------")
+  print("1. (B)ook Management")
+  print("2. Bo(r)rower Management")
+  print("3. (F)inance Management")
+  print("4. (A)ccount Management")
+  print("5. (H)elp and support")
+  return(input("==========\n Your choice:"))
