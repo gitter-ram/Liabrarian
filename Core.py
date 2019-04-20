4265efdddd3b8fbe4b4d410dc3ddc6b363dbaa91
Gitter <40431840+gitter-ram@users.noreply.github.com>
2019-04-20 14:43:58 +0530

    Update Core.py

--- Core.py
+++ Core.py
@@ -28,3 +28,56 @@ def encryptFile(file, passwd):
     if(fh != None):
       fh.close()
   return(0)
+
+def decryptFile(file, passwd, alm)
+  '''The decryption routine for encrypted files
+     Care must be taken that 'file' and 'alm' must have the 
+     File name in absolute path'''
+  #Check alm
+  if not os.access(alm, R_OK):
+    return(-3) #Alm unreadable
+  pass_match = False
+  try:
+    falm = open(alm, "r")
+    while (am = (falm.readline)) != "":
+      if(am.endswith("\n")):
+        a = eval(am[0:-1])
+      else:
+        a = eval(am)
+      if (file in a):
+        if a[file] == passwd)
+          pass_match = True
+        else:
+          pass_match = False
+          break
+    if not pass_match:
+      return(-4) #Passwords do not match
+    
+    #check if the source file is readable
+    if not os.access(file, R_OK)
+      return(-1) #Source file is unreadable
+    fh = open(file, "r")
+    buff = 10
+    
+    #remove the destination fike if it exists
+    if (os.access(file[0:-4], F_OK)):
+      os.remove(file[0:-4])
+    fho = open(file[0:-4], "w")
+    
+    while (a = (fh.read(buff))) != "" :
+      for j in a:
+        c = ord(j)
+        c = ~(c ^ 0xAA)
+        c = chr(c)
+        fho.write(c)
+  except (Exception):
+    return(-2) #io error
+  finally:
+    if falm != None:
+      falm.close()
+    if fho != None:
+      fho.close()
+    if fh != None:
+      fh.close()
+  return(0) #Success
+  
