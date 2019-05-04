31631a30bd5f2153f701c709dcc00eab1a0d37ce
Gitter <40431840+gitter-ram@users.noreply.github.com>
2019-05-04 13:46:06 +0530

    Update FileDisplay.py

--- FileDisplay.py
+++ FileDisplay.py
@@ -3,7 +3,11 @@ class FilePlayground:
   SELECTION = []
   PERMITTED_ICON_SIZES = (16,32,48,256)
   CURR_ICON_SIZE = 32
-  def __init__(parent, iconsize=32):
+  def __init__(parent, height, width, items, iconsize=32):
     #First, initialize the canvas and some icons
     CURR_ICON_SIZE = iconsize
-    self.canv = tk.Canvas(
+    self.canv = tk.Canvas(master, width=width, height=height)
+    self.canv.pack()
+    #add the icons and their discriptions
+    for i in items:
+      #Load the icons:
