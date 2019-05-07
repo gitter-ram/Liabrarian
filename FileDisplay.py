9ffdbca992fd2cbd983f514afa20732f3d6158f4
Gitter <40431840+gitter-ram@users.noreply.github.com>
2019-05-07 17:31:27 +0530

    Update FileDisplay.py

--- FileDisplay.py
+++ FileDisplay.py
@@ -19,7 +19,7 @@ class FilePlayground:
           ICON = os.path.join(configdir, ("\\icons\\current theme\\" + i + ".gif"))
         else:
           ICON = os.path.join(configdir, ("\\icons\\current theme\\" + i[(i.find('.') + 1):] + ".gif"))
-      ICON_IMG.update({i:tk.PhotoImage(file=ICON)})
+      ICON_IMG.update({i[((i.find('.')+1):]:tk.PhotoImage(file=ICON)})
     #start rendering the canvas:
     #get the height and the width of the parent
     tk.update()
@@ -42,19 +42,27 @@ class FilePlayground:
     self.grid = [(self.canv_width // (self.ICON_SIZE + self.ICON_SPACING)), (self.canv_height // (self.ICON_SIZE + self.ICON_SPACING))]
     self.updateCanvas(items, nodelete=True)
   
-  def updateCanvas(items, nodelete=False):
+  def updateCanvas(items, selected, nodelete=False):
     #Delete all the items on the canvas first:
     if not nodelete:
       self.canv.delete(self.objectsDrawn) #objectsdrawn is a list of all the objects that were drawn.
     #Start drawing the objects
     j = 0
     TEXT_ICON_SPACE = 5
-    NUM_COLUMNS = self.parent.winfo_width() // (self.ICON_SIZE + (2 * self.ICON_SPACING))
+    NUM_COLUMNS = self.parent.winfo_width() // (self.ICON_SIZE + (4 * self.ICON_SPACING))
     NUM_ROWS = self.parent.winfo_height() // (self.ICON_SIZE + (2 * self.ICON_SPACING))
     for i in range(0, NUM_ROWS):
       for j in range(0, NUM_COLUMNS):
         x = (j * (self.ICON_SIZE + self.ICON_SPACING))
-        y = (i * (self.ICON_SIZE + self.ICON_SPACING))
+        y = (i * (self.ICON_SIZE + 2 * self.ICON_SPACING))
         alpha = i * NUM_COLUMNS + j
-        #Find the appropriate icon
+        #Find the appropriate icons and display them.
+        if selected[items[alpha]] == True:
+          #draw a blue rectangle around the icon:
+          objectsDrawn.append(canv.create_rectangle((x - (ICON_SPACING // 2), (y - (ICON_SPACING // 2)), (x + ICON_SIZE + (ICON_SPACING // 2)), (y + ICON_SIZE + (ICON_SPACING // 2)), fill='blue') 
+        extension =(iems[alpha])[(items[alpha].find('.') + 1):]
+        objectsDrawn.append(canv.create_image(x, y, self.ICON_IMG[extension]))
+        #display the names as well.
+        objectsDrawn.append(canv.create_text((x + 5), (y + self.ICON_SIZE + self.ICON_SPACING), text=items[alpha])
         
+         
